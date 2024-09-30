9ae13d9be6a0754d4c551fd60a569ffe02183b1a
Fabian Palacio <fabianalfonsopalacio27@gmail.com>
2024-09-30 14:58:12 -0500

    Cambios control 3

--- HMI/Control3.Designer.cs
+++ HMI/Control3.Designer.cs
@@ -28,19 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.btnConectar = new System.Windows.Forms.Button();
+            this.BtnConectar = new System.Windows.Forms.Button();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.panel1.Location = new System.Drawing.Point(420, 132);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(200, 193);
@@ -48,22 +51,27 @@
             // 
             // panel2
             // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.panel2.Controls.Add(this.chart1);
             this.panel2.Location = new System.Drawing.Point(12, 12);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(402, 313);
             this.panel2.TabIndex = 1;
             // 
-            // btnConectar
+            // BtnConectar
             // 
-            this.btnConectar.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.btnConectar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnConectar.Location = new System.Drawing.Point(432, 43);
-            this.btnConectar.Name = "btnConectar";
-            this.btnConectar.Size = new System.Drawing.Size(160, 44);
-            this.btnConectar.TabIndex = 2;
-            this.btnConectar.Text = "Conectar";
-            this.btnConectar.UseVisualStyleBackColor = false;
+            this.BtnConectar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BtnConectar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnConectar.ForeColor = System.Drawing.SystemColors.Control;
+            this.BtnConectar.Location = new System.Drawing.Point(432, 43);
+            this.BtnConectar.Name = "BtnConectar";
+            this.BtnConectar.Size = new System.Drawing.Size(160, 44);
+            this.BtnConectar.TabIndex = 2;
+            this.BtnConectar.Text = "Conectar";
+            this.BtnConectar.UseVisualStyleBackColor = false;
+            this.BtnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            this.BtnConectar.Enter += new System.EventHandler(this.btnConectar_Enter);
+            this.BtnConectar.Leave += new System.EventHandler(this.btnConectar_Leave);
             // 
             // chart1
             // 
@@ -71,7 +79,7 @@
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(3, 10);
+            this.chart1.Location = new System.Drawing.Point(14, 17);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
@@ -80,20 +88,27 @@
             series1.Legend = "Legend1";
             series1.Name = "Analog";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(396, 300);
+            this.chart1.Size = new System.Drawing.Size(374, 283);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
             // 
             // Control3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(632, 337);
-            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.BtnConectar);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Control3";
             this.Text = "Control3";
+            this.Load += new System.EventHandler(this.Control3_Load);
             this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
@@ -104,7 +119,8 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button BtnConectar;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.IO.Ports.SerialPort serialPort1;
     }
 }
--- HMI/Control3.cs
+++ HMI/Control3.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CircularProgressBar;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,64 @@ namespace HMI
         public Control3()
         {
             InitializeComponent();
+        }
+
+      
+
+        private void Control3_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Control3_FormClosing(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+
+        }
+
+        private void BtnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnConectar_Enter(object sender, EventArgs e)
+        {
+            BtnConectar.BackColor = Color.Gray;
+        }
+
+        private void BtnConectar_Leave(object sender, EventArgs e)
+        {
+            BtnConectar.BackColor = Color.DodgerBlue;
+        }
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            while (serialPort1.IsOpen && serialPort1.BytesToRead > 0)
+            {
+                string serialData = serialPort1.ReadLine();
+
+                int value = Convert.ToInt32(serialData);
+
+                //if (value >= CircularProgressBar1.Minimum &&
+                //    value <= CircularProgressBar1.Maximum)
+                //{
+                //    CircularProgressBar.invoke((MethodInvoker)(() =>
+                //    {
+                //        CircularProgressBar1.Text = serialData;
+                //        CircularProgressBar1.Value = value;
+                //    }));
+                //}
+                chart1.Invoke((MethodInvoker)(() => chart1.Series
+                ["Analog"].Points.Add(value)));
+            }
         }
     }
 }
